ee0a0ef529b90a538930a6e4f03deb772d58eae6
aroerick <a.roerick@gmail.com>
2018-08-30 15:21:42 -0700

    Style menuBar

--- client/src/components/MenuBar/MenuBar.js
+++ client/src/components/MenuBar/MenuBar.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import MenuItem from '@material-ui/core/MenuItem'
-import Menu from '@material-ui/core/Menu'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  MenuItem,
+  Menu,
+  Button
+} from '@material-ui/core/'
+import MenuIcon from '@material-ui/icons/MoreVert'
+import AddCircle from '@material-ui/icons/AddCircle'
 import Logo from '../../images/boomtown.svg'
 import { Link } from 'react-router-dom'
 import AuthContainer from '../../containers/AuthContainer'
-
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  flex: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-}
+import styles from './styles'
 
 class MenuBar extends Component {
   state = {
@@ -50,51 +42,60 @@ class MenuBar extends Component {
     return (
       <div className={classes.root}>
         <AppBar position="static">
-          <Toolbar>
+          <Toolbar className={classes.toolBar}>
             <Link to="/">
-              <img src={Logo} alt="Boomtown Logo" />
+              <Button className={classes.menuButton}>
+                <img src={Logo} alt="Boomtown Logo" height="50" />
+              </Button>
             </Link>
-            <Link to="/share">
-              <Typography
-                variant="title"
-                color="inherit"
-                className={classes.flex}
-              >
-                SHARE SOMETHING
-              </Typography>
-            </Link>
-            <div>
-              <IconButton
-                aria-owns={open ? 'menu-appbar' : null}
-                aria-haspopup="true"
-                onClick={this.handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                open={open}
-                onClose={this.handleClose}
-              >
-                <Link to="/profile">
-                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                </Link>
-                <AuthContainer>
-                  {({ logout }) => (
-                    <MenuItem onClick={values => {logout.mutation()}}>Sign Out</MenuItem>
-                  )}
-                </AuthContainer>
-              </Menu>
+            <div className={classes.navOptions}>
+              <Link to="/share">
+                <Button>
+                  <AddCircle />
+                  <Typography color="inherit" className={classes.flex}>
+                    SHARE SOMETHING
+                  </Typography>
+                </Button>
+              </Link>
+              <div className={classes.navMenu}>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <Link to="/profile">
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  </Link>
+                  <AuthContainer>
+                    {({ logout }) => (
+                      <MenuItem
+                        onClick={values => {
+                          logout.mutation()
+                        }}
+                      >
+                        Sign Out
+                      </MenuItem>
+                    )}
+                  </AuthContainer>
+                </Menu>
+              </div>
             </div>
           </Toolbar>
         </AppBar>
---
+++ client/src/components/MenuBar/styles.js
@@ -0,0 +1,23 @@
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flexGrow: 1
+  },
+  menuButton: {
+    flex: 1
+  },
+  navOptions: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap'
+  },
+  navMenu: {
+    display: 'inline'
+  },
+  toolBar: {
+    justifyContent: 'space-between'
+  }
+}
+export default styles
