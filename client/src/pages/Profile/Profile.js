efbad6f60266d562182bba0a20fc1915da8f2291
aroerick <a.roerick@gmail.com>
2018-08-31 13:10:28 -0700

    Style profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,48 +1,69 @@
 import { withStyles } from '@material-ui/core/styles'
 import React from 'react'
 import ItemsContainer from '../../containers/ItemsContainer'
-import { Grid, Card, Typography, CardContent } from '@material-ui/core'
+import {
+  Grid,
+  Card,
+  Typography,
+  CardContent,
+  CardHeader
+} from '@material-ui/core'
 import ItemCard from '../../components/ItemCard'
+import Gravatar from 'react-gravatar'
 
 import styles from './styles'
 
 const Profile = ({ classes }) => {
   return (
-    <div>
-      <Grid container spacing={24} className={classes.root}>
-        <ItemsContainer>
-          {({ userItemsData: { data, loading, error } }) => {
-            if (loading) {
-              return 'Content Loading...'
-            }
-            if (error) {
-              return `error: ${error.message}`
-            }
-            return (
-              <div>
-                <Card className={classes.card}>
-                  <CardContent>
-                    <Typography variant="headline" component="h2">
-                      {data.user.fullname}
-                    </Typography>
-                    <Typography>
-                      {data.user.items.length} Items shared{' '}
-                      {data.user.borrowed.length} Items Borrowed
-                    </Typography>
-                    <Typography>{data.user.bio}</Typography>
-                  </CardContent>
-                </Card>
-                {data.user.items.map(item => (
-                  <Grid key={item.id} item xs={12} sm={6} lg={4}>
-                    <ItemCard item={item} />
-                  </Grid>
-                ))}
-              </div>
-            )
-          }}
-        </ItemsContainer>
-      </Grid>
-    </div>
+    <ItemsContainer>
+      {({ userItemsData: { data, loading, error } }) => {
+        if (loading) {
+          return 'Content Loading...'
+        }
+        if (error) {
+          return `error: ${error.message}`
+        }
+        return (
+          <Grid
+            container
+            spacing={16}
+            direction="column"
+            className={classes.root}
+          >
+            <Grid item xs={12} className={classes.headerCard}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Gravatar
+                    className={classes.avatar}
+                    email={data.user.email}
+                  />
+                  <Typography className={classes.name} variant="display3" component="h2">
+                    {data.user.fullname}
+                  </Typography>
+                  <Typography variant='headline'>
+                    <span className={classes.bold}>{data.user.items.length} </span>Items shared{' '}
+                    <span className={classes.bold}>{data.user.borrowed.length}</span> Items Borrowed
+                  </Typography>
+                  <Typography>{data.user.bio}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography color="primary" variant="display1">
+                Shared Items
+              </Typography>
+            </Grid>
+            <Grid container spacing={24}>
+              {data.user.items.map(item => (
+                <Grid key={item.id} item xs={12} sm={6} lg={4}>
+                  <ItemCard item={item} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        )
+      }}
+    </ItemsContainer>
   )
 }
 
--- client/src/pages/Profile/styles.js
+++ client/src/pages/Profile/styles.js
@@ -1,3 +1,34 @@
-const styles = theme => ({})
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    minHeight: '100%',
+    background: theme.palette.secondary.main,
+    [theme.breakpoints.up('xs')]: {
+      padding: theme.spacing.unit * 4,
+      justifyContent: 'flex-start'
+    },
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing.unit * 8,
+    },
+    margin: 0,
+  },
+  headerCard: {
+    marginTop: 10,
+  },
+  card: {
+    padding: 30,
+  },
+  avatar: {
+    display: 'inline',
+    borderRadius: '50%',
+  },
+  name: {
+    display: 'inline',
+    paddingLeft: 30,
+  },
+  bold : {
+    fontWeight: 'bold',
+  }
+})
 
 export default styles
